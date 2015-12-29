8f4e8905ee0eca53b52db80351d5c4749d1dedc1
Mike <msjarrett@gmail.com>
2015-12-28 18:33:48 -0800

    day 8 part 1 and 2

--- day8/day8.py
+++ day8/day8.py
@@ -21,6 +21,7 @@ def count_total_chars(line):
 def count_mem_chars(line):
     raw_length = len(line)
     mem_length = raw_length-2
+    coded_length = raw_length+4
 
     match = re.findall(r'\\(.)',line)
     
@@ -29,28 +30,34 @@ def count_mem_chars(line):
         if m == 'x':
             print 'hex'
             mem_length -= 3
+            coded_length += 1
         elif m == '"':
             print 'quote'
             mem_length -= 1
+            coded_length += 2
         else:
             print 'bslash'
             mem_length -= 1
-    print raw_length
-    print mem_length
-    return raw_length, mem_length
+            coded_length += 2
+
+    return raw_length, mem_length, coded_length
 
 
 
 total_raw_length = 0
 total_mem_length = 0
+total_coded_length = 0
 for line in f:
     line = line.rstrip('\n') # removes trailing \n
     print line
     #count_total_chars(line)
-    line_raw_length, line_mem_length = count_mem_chars(line)
+    line_raw_length, line_mem_length, line_coded_length = count_mem_chars(line)
     total_raw_length += line_raw_length
     total_mem_length += line_mem_length
+    total_coded_length += line_coded_length
 
 print total_raw_length
 print total_mem_length
-print total_raw_length - total_mem_length 
+print total_coded_length
+print "part 1: " + str(total_raw_length - total_mem_length)
+print "part 2: " + str(total_coded_length - total_raw_length)
