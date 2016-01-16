2498c15b7180094b2519d87ecf07d3c4859a4797
Mike <msjarrett@gmail.com>
2016-01-16 15:19:28 -0800

    just need to add winning condition

--- day22/day22.py
+++ day22/day22.py
@@ -21,18 +21,20 @@ poison =  [4,173,6,3,0,0,0]
 recharge =[5,229,5,0,0,0,101]
 spells =  [missile,drain,shield,poison,recharge]
 
+effects = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
 
+results = {}
 #provide hitpoints, damage given and defense for each player     
 def fight(mhp,mdam,mdef,bhp,bdam,bdef):
 
-    mhp -= max([bdam - mdef,1])
     bhp -= max([mdam - bdef,1])
+    mhp -= max([bdam - mdef,1])
 
     return mhp,bhp
 
 def init():
-    # each element of future should be a list [damage dealt, armour received, health received, newmana]
-    future = [[0]*4,[0]*4,[0]*4,[0]*4,[0]*4,[0]*4,]    
+    # each element of future should be a list [damage dealt, armour received, health received, newmana]. Max effect length is 6 turns, so only need to keep track of next 6 turns
+    future = [[0]*4,[0]*4,[0]*4,[0]*4,[0]*4,[0]*4]    
 
     # initialize stats
     myhp = 50
@@ -43,67 +45,87 @@ def init():
     return future, [myhp,mymana,bosshp,bossdam] 
 
 def turn(future, stats):
-    #choose a random spell
-    goodspells = [x for x in spells if x[1] <= stats[1]]  #only choose from spells you can afford
-    
-    try:
-        spell = random.choice(goodspells)
-        print "spell %d" % (spell[0])
-        print spell
-    except IndexError:
-        spell = [0]*7
-        print "Out of mana"
 
+
+    print "--------------------"
+    print "--------------------"
+    
+
+#    print effects
+    #choose a random spell
+    goodspells = [ x for x in spells if x[1] <= stats[1] ]  #only choose from spells you can afford
+    goodspells = [ x for x in goodspells if effects[x[0]] <= 1 ] # and spells that aren't currently ongoing
+
+#    print "active spells: "
+#    print [ x[0] for x in spells if effects[x[0]] >0 ]
+
+
+    spell = random.choice(goodspells)
+    print "spell %d" % (spell[0])
+#    print spell
+    effects[spell[0]] = spell[2] 
+#    print effects
+    
+        
 
     #add to future according to length of effect
     for i in range(spell[2]):
-        #print i
+
         future[i][0] += spell[3]  #damage
         future[i][1] += spell[4]  #armour
         future[i][2] += spell[5]  #health
         future[i][3] += spell[6]  #newmana
-        
 
-    print stats
-    print future
+#    print future
+#    print stats
+
     #run fight(mhp,mdam,mdef,bhp,bdam,bdef)
     mhp,bhp = fight(stats[0],future[0][0],future[0][1],stats[2],stats[3],0)
-    print "hitpoints after round %d, %d" % (mhp, bhp)
+#    print "hitpoints after round %d, %d" % (mhp, bhp)
     mymana = stats[1] - spell[1]
-    print "mana after spending it: %d" % mymana
+#    print "mana after spending it: %d" % mymana
     mymana += future[0][3]
-    print "mana after recharge: %d" % mymana
+#    print "mana after recharge: %d" % mymana
     mhp += future[0][2]
-    print "hp after health boost: %d" % mhp
+#    print "hp after health boost: %d" % mhp
 
     
     #update future list mylist.insert(0, mylist.pop(5))
-    future.pop(0) #remove first elemet from list
+    future.pop(0) #remove first element from list
     future.append([0]*4) #add a 6th day to the end of the list
+#    print future
+
+    # update effects
+    for k in effects.keys():
+        effects[k] = max(effects[k]-1, 0)
 
     #update stats
     stats = [mhp,mymana,bhp,stats[3]]
 
-    return future, stats
+    if stats[1] < 53 or mhp <= 0 or bhp <= 0:
+        return future, stats, True, spell[0], spell[1]
+    else:
+        return future, stats, False, spell[0], spell[1]
 
 def sim_match():
     #initialize stats
     f,s = init()
-    #print turn(f,s)
+    isOver = False
+    spellorder = ''
+    manaspent = 0
+
+    while not isOver:
+        f,s, isOver, lastSpell, mana= turn(f,s)
+        spellorder += str(lastSpell)
+        manaspent += mana
+        #print s
+    #print spellorder
+    return spellorder, manaspent
     
-    while s[0]>0:
-        f,s = turn(f,s)
-        print s
-    return s
+for i in range(100):
+    matches = {}
+    a, b = sim_match()
+    matches[a] = b
 
-
-sim_match()
-
-#while True:
-#    s = sim_match()
-
-#    if s[2] < 0:
-#        print s
-#        break
-
+print matches
 
